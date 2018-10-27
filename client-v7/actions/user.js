b33a7f3b40e3a2e6e1fe017abfbe5c54a0b76620
Tina Howard <chh2538@rit.edu>
2018-10-27 00:25:22 -0400

    Add default emoji for user if no username exists

--- client-v7/actions/user.js
+++ client-v7/actions/user.js
@@ -1,4 +1,11 @@
-const defaultName = typeof localStorage !== 'undefined' ? localStorage.getItem('name') : ''
+const EmojiConverter = require('emoji-js')
+
+const converter = new EmojiConverter()
+
+const emojis = [':poop:', ':alien:', ':ghost:', ':tram:', ':floppy_disk:', ':tada:', ':confetti_ball:', ':coffee:', ':vhs:', ':tophat:', ':unicorn_face:', ':octopus:', ':popcorn:', ':100:', ':fax:', ':hourglass:']
+// const emojiMap = emojis.map(string => converter.replace_colons(string)) // For testing purposes
+const emoji = converter.replace_colons(emojis[Math.floor(Math.random() * emojis.length)])
+const defaultName = (typeof localStorage !== 'undefined') && (localStorage.getItem('name')) ? localStorage.getItem('name') : emoji
 
 module.exports = {
   init: () => defaultName,
