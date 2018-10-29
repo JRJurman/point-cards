b5b46ecaf13ba4b21ece2a5982e2406d9c8f8be8
jjurman <jjurman@constantcontact.com>
2018-10-28 22:17:39 -0400

    remove emoji logic temporarily

--- client-v7/actions/user.js
+++ client-v7/actions/user.js
@@ -1,11 +1,12 @@
-const EmojiConverter = require('emoji-js')
+// Emoji logic commented out until work is done to fix mobile
+// const EmojiConverter = require('emoji-js')
 
-const converter = new EmojiConverter()
+// const converter = new EmojiConverter()
 
-const emojis = [':poop:', ':alien:', ':ghost:', ':tram:', ':floppy_disk:', ':tada:', ':confetti_ball:', ':coffee:', ':vhs:', ':tophat:', ':unicorn_face:', ':octopus:', ':popcorn:', ':100:', ':fax:', ':hourglass:']
+// const emojis = [':poop:', ':alien:', ':ghost:', ':tram:', ':floppy_disk:', ':tada:', ':confetti_ball:', ':coffee:', ':vhs:', ':tophat:', ':unicorn_face:', ':octopus:', ':popcorn:', ':100:', ':fax:', ':hourglass:']
 // const emojiMap = emojis.map(string => converter.replace_colons(string)) // For testing purposes
-const emoji = converter.replace_colons(emojis[Math.floor(Math.random() * emojis.length)])
-const defaultName = (typeof localStorage !== 'undefined') && (localStorage.getItem('name')) ? localStorage.getItem('name') : emoji
+// const emoji = converter.replace_colons(emojis[Math.floor(Math.random() * emojis.length)])
+const defaultName = (typeof localStorage !== 'undefined') && (localStorage.getItem('name')) ? localStorage.getItem('name') : ''
 
 module.exports = {
   init: () => defaultName,
